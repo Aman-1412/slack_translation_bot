b0627fa01d63a7ceb14efcb32b5a787d79977fbf
Aman Goyal <amangoyal1412@gmail.com>
2020-09-09 21:12:23 +0530

    Added interactivity response and user language mapping

--- app/slack_bot_2.py
+++ app/slack_bot_2.py
@@ -6,8 +6,9 @@ import logging
 from pathlib import Path
 
 
-from flask import Flask , jsonify
+from flask import Flask , jsonify, request
 from slack import WebClient
+from slack.errors import SlackApiError
 from slackeventsapi import SlackEventAdapter
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
@@ -16,7 +17,10 @@ load_dotenv(find_dotenv())
 LOG_FILE = f'{Path(__file__).absolute().parent.parent}/logs/{Path(__file__).name}-file-{time.time()}.log'
 print(LOG_FILE)
 # Create a .env file containing the following details
-
+SLACK_CLIENT_ID=os.environ['SLACK_CLIENT_ID']
+SLACK_CLIENT_SECRET=os.environ['SLACK_CLIENT_SECRET']
+SLACK_SIGNING_SECRET=os.environ['SLACK_SIGNING_SECRET']
+SLACK_BOT_ACCESS_TOKEN=os.environ['SLACK_BOT_ACCESS_TOKEN']
 
 
 
@@ -28,14 +32,21 @@ slack_events_adapter = SlackEventAdapter(SLACK_SIGNING_SECRET, "/slack/events", 
 # Initialize a Web API client
 slack_web_client = WebClient(token=SLACK_BOT_ACCESS_TOKEN)
 
+try:
+    with open(f'{Path(__file__).parent}/user_language_mapping.json') as f:
+        USER_LANGUAGE_MAPPINGS = json.load(f)
+except FileNotFoundError:
+    USER_LANGUAGE_MAPPINGS = {}
+    logging.info("First run?")
+
 #when the app starts, load the language_mappings.json file and create a json for /change_language command. Any changes to language list to just be done
 #in language_mappings.json file
-with open('language_mapping.json') as language:
+with open(f'{Path(__file__).parent}/language_mapping.json') as language:
     language_mappings = json.load(language)
 # print("Type:", type(language_mappings))
 jsonres = {
     "text": "Please choose your preferred language",
-    "response_type": "in_channel",
+    "response_type": "ephemeral",
     "attachments": [
         {
             "text": "Select a language",
@@ -58,10 +69,38 @@ for(key) in language_mappings:
     jsonres["attachments"][0]["actions"][0]["options"].append({"text":key, "value":language_mappings[key]})
 
 
+def get_language_from_iso_code(iso_code):
+    for k, v in language_mappings.items():
+        if v == iso_code:
+            return k
+
 #endpoint for /change_language
 @app.route('/slack/event/change_language', methods=['POST'])
 def changelang():
     print("In change lang funcv")
+    return jsonify(jsonres)
+
+@app.route('/slack/event/valueSelected', methods=['POST'])
+def valueSelected():
+    global USER_LANGUAGE_MAPPINGS
+    payload =  request.form.get('payload')
+    print("Type of Payload " + str(type(payload)))
+    payload = json.loads(payload)
+    print(payload)
+    channelId = payload.get('channel').get('id')
+    print('channelId ' + channelId)  
+    selected_language_iso_code=payload.get('actions')[0].get('selected_options')[0].get('value')
+    print("Selected Value is " + selected_language_iso_code)
+    userId = payload.get('user').get('id')
+    print('userId ' + userId)
+    USER_LANGUAGE_MAPPINGS.update({userId: selected_language_iso_code})
+    with open(f'{Path(__file__).parent}/user_language_mapping.json', 'w') as fp:
+        json.dump(USER_LANGUAGE_MAPPINGS, fp, sort_keys=True, indent=4)
+    selected_language_text = get_language_from_iso_code(selected_language_iso_code)
+    jsonres = {
+    "text": f"Thank you {userId}. Your Language has been set to {selected_language_text}"
+    }
+
     return jsonify(jsonres)
 
 @slack_events_adapter.on("message")
